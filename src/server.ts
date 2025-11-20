24dd8db06e8e24212ee1d0a9dbd941f283a3e673
Hanson Zhuang <hansonzhuang@outlook.com>
2025-11-20 12:25:31 +0000

    change model

--- src/server.ts
+++ src/server.ts
@@ -13,12 +13,14 @@ import {
   createUIMessageStreamResponse,
   type ToolSet
 } from "ai";
-import { openai } from "@ai-sdk/openai";
+import { createWorkersAI } from "workers-ai-provider";
 import { processToolCalls, cleanupMessages } from "./utils";
 import { tools, executions } from "./tools";
-// import { env } from "cloudflare:workers";
+import { env } from "cloudflare:workers";
 
-const model = openai("gpt-4o-2024-11-20");
+const workersai = createWorkersAI({ binding: env.AI });
+
+const model = workersai("@cf/meta/llama-3-8b-instruct");
 // Cloudflare AI Gateway
 // const openai = createOpenAI({
 //   apiKey: env.OPENAI_API_KEY,
